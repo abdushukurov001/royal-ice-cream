f91da24999bb0993076b416ea7aad76e1eec7441
Abdulmajid78 <jackodilov@gmail.com>
2025-02-15 18:59:33 +0500

    category

--- src/components/ProductDetail.jsx
+++ src/components/ProductDetail.jsx
@@ -1,143 +1,146 @@
-import React, { useState, useEffect } from 'react';
-import ice1 from '../assets/ice1.png';
+import {useState, useEffect} from 'react';
+import {useParams} from 'react-router-dom';
 import Navbar from "./Navbar.jsx";
-import { useTranslation } from "react-i18next";
+import {useTranslation} from "react-i18next";
 import Footer from "./Footer.jsx";
-import ProductModal from './ProductDetailModal.jsx'; // Modal komponentini import qilamiz
-
-const products = [
-  {
-    id: 1,
-    name: "Zarli",
-    description: "  tg twrtgrtgr gtrgtretg trgre tgrg ertegretgtgrg  ",
-    image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSMsEqWI5dkAVDpJ7qm27MKhnQlZfWaJBP4fQ&s',
-  },
-  {
-    id: 2,
-    name: "Коровка из Кореновки",
-    description: "Sample of brownie",
-    image: 'https://zira.uz/wp-content/uploads/2018/07/morojenoe-korovka-2.jpg',
-  },
-  {
-    id: 3,
-    name: "Stakan",
-    description: "Sample of brownie",
-    image: 'https://zira.uz/wp-content/uploads/2018/07/morojenoe-korovka-2.jpg',
-  },
-  {
-    id: 5,
-    name: "Коровка из Кореновки",
-    description: "Sample of brownie",
-    image: 'https://zira.uz/wp-content/uploads/2018/03/icecream-2.jpg',
-  },
-];
+import ProductModal from './ProductDetailModal.jsx';
+import client from "../service/index.jsx";
+import Loading from "./Loading.jsx";
 
 const ProductDetail = () => {
+    const {category} = useParams(); // Get category from the URL
+    const {t} = useTranslation();
+    const [products, setProducts] = useState([]);
+    const [selectedProduct, setSelectedProduct] = useState(null);
+    const [loading, setLoading] = useState(true)
+    // Fetch products based on the category
+    useEffect(() => {
+        const fetchProducts = async () => {
+            try {
+                const response = await client.get(`/products?category=${category}`); // Fetch products from API
+                setProducts(response.data); // Assuming the response contains an array of products
+                setLoading(false)
+                console.log(response.data)
+            } catch (error) {
+                console.error('Error fetching products:', error);
+            }
+        };
+
+        if (category) {
+            fetchProducts();
+        }
+    }, [category]);
+
+    const handleProductClick = (product) => {
+        setSelectedProduct(product);
+    };
+
+    const closeModal = () => {
+        setSelectedProduct(null);
+    };
 
     useEffect(() => {
-            window.scrollTo({ top: 0, behavior: 'smooth' });
-        }, []);
+        window.scrollTo({top: 0, behavior: 'smooth'});
+    }, []);
 
-  const { t } = useTranslation();
-  const [selectedProduct, setSelectedProduct] = useState(null);
+    return (
+        <>
+            <Navbar/>
+            <div className="min-h-screen bg-gray-50 py-8 mt-23 px-4 sm:px-6 lg:px-8">
+                <div className="max-w-7xl mx-auto">
 
-  const handleProductClick = (product) => {
-    setSelectedProduct(product);
-  };
 
-  const closeModal = () => {
-    setSelectedProduct(null);
-  };
-
-  return (
-    <>
-      <Navbar />
-      <div className="min-h-screen bg-gray-50 py-8 mt-23 px-4 sm:px-6 lg:px-8">
-        <div className="max-w-7xl mx-auto">
-          <div className="breadcrumb flex">
-            <a
-              href="/"
-              className="mb-6 me-2 inline-flex items-center text-gray-600 hover:text-gray-900 transition-colors"
-            >
-              Home
-            </a>
-            <a
-              onClick={() => window.history.back()}
-              className="mb-6 inline-flex cursor-pointer items-center text-gray-600 hover:text-gray-900 transition-colors"
-            >
-              <svg
-                className="w-8 h-5 mr-1"
-                fill="none"
-                stroke="currentColor"
-                viewBox="0 0 24 24"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={2}
-                  d="M5 12h14m-7-7l7 7-7 7"
-                />
-              </svg>
-              Products catalog
-            </a>
-            <a
-              onClick={() => window.history.back()}
-              className="mb-6 inline-flex font-bold cursor-pointer items-center text-gray-600 hover:text-gray-900 transition-colors"
-            >
-              <svg
-                className="w-8 h-5 mr-1"
-                fill="none"
-                stroke="currentColor"
-                viewBox="0 0 24 24"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={2}
-                  d="M5 12h14m-7-7l7 7-7 7"
-                />
-              </svg>
-              Products Info
-            </a>
-          </div>
-
-          <div className="space-y-12">
-            {[1].map((category) => (
-              <section key={category}>
-                <h2 className="text-3xl font-semibold mb-4">Chopli</h2>
-                <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-4">
-                  {products.map((product) => (
-                    <div
-                      key={product.id}
-                      onClick={() => handleProductClick(product)}
-                      className="bg-[#FFF5F7] rounded-lg p-4 transition-transform hover:scale-105 cursor-pointer"
-                    >
-                      <div className="aspect-square mx-auto relative mb-3 rounded-lg overflow-hidden">
-                        <img
-                          src={product.image}
-                          alt={product.name}
-                          width={260}
-                          height={200}
-                          className="object-cover h-60"
-                        />
-                      </div>
-                      <h3 className="text-md font-medium text-[#FF1493]">{product.name}</h3>
-                      <p className="text-sm text-gray-500  line-clamp-2">{product.description}</p>
-                    </div>
-                  ))}
+                    {loading ? <Loading/> :
+                        <>
+                            <div className="breadcrumb flex">
+                                <a
+                                    href="/"
+                                    className="mb-6 me-2 inline-flex items-center text-gray-600 hover:text-gray-900 transition-colors"
+                                >
+                                    Home
+                                </a>
+                                <a
+                                    onClick={() => window.history.back()}
+                                    className="mb-6 inline-flex cursor-pointer items-center text-gray-600 hover:text-gray-900 transition-colors"
+                                >
+                                    <svg
+                                        className="w-8 h-5 mr-1"
+                                        fill="none"
+                                        stroke="currentColor"
+                                        viewBox="0 0 24 24"
+                                    >
+                                        <path
+                                            strokeLinecap="round"
+                                            strokeLinejoin="round"
+                                            strokeWidth={2}
+                                            d="M5 12h14m-7-7l7 7-7 7"
+                                        />
+                                    </svg>
+                                    Products catalog
+                                </a>
+                                <a
+                                    onClick={() => window.history.back()}
+                                    className="mb-6 inline-flex font-bold cursor-pointer items-center text-gray-600 hover:text-gray-900 transition-colors"
+                                >
+                                    <svg
+                                        className="w-8 h-5 mr-1"
+                                        fill="none"
+                                        stroke="currentColor"
+                                        viewBox="0 0 24 24"
+                                    >
+                                        <path
+                                            strokeLinecap="round"
+                                            strokeLinejoin="round"
+                                            strokeWidth={2}
+                                            d="M5 12h14m-7-7l7 7-7 7"
+                                        />
+                                    </svg>
+                                    Products Info
+                                </a>
+                            </div>
+                            <div className="space-y-12">
+                                <section>
+                                    {products.length > 0 && (
+                                        <h2 className="text-3xl font-semibold mb-4">{products[0].category.title}</h2>
+                                    )}
+                                    <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-4">
+                                        {products.length > 0 ? (
+                                            products.map((product) => (
+                                                <div
+                                                    key={product.id}
+                                                    onClick={() => handleProductClick(product)}
+                                                    className="bg-[#FFF5F7] rounded-lg p-4 transition-transform hover:scale-105 cursor-pointer"
+                                                >
+                                                    <div
+                                                        className="aspect-square mx-auto relative mb-3 rounded-lg overflow-hidden">
+                                                        <img
+                                                            src={product.image}
+                                                            alt={product.name}
+                                                            width={260}
+                                                            height={200}
+                                                            className="object-cover h-60"
+                                                        />
+                                                    </div>
+                                                    <h3 className="text-md font-medium text-[#FF1493]">{product.name}</h3>
+                                                    <p className="text-sm text-gray-500  line-clamp-2">{product.description}</p>
+                                                </div>
+                                            ))
+                                        ) : (
+                                            <p>No products found in this category.</p>
+                                        )}
+                                    </div>
+                                </section>
+                            </div>
+                        </>}
                 </div>
-              </section>
-            ))}
-          </div>
-        </div>
-      </div>
-      <Footer />
+            </div>
+            <Footer/>
 
-      {selectedProduct && (
-        <ProductModal product={selectedProduct} onClose={closeModal} />
-      )}
-    </>
-  );
+            {selectedProduct && (
+                <ProductModal product={selectedProduct} onClose={closeModal}/>
+            )}
+        </>
+    );
 };
 
 export default ProductDetail;
--- src/components/ProductDetailModal.jsx
+++ src/components/ProductDetailModal.jsx
@@ -1,54 +1,52 @@
-import React from 'react';
-import { FaTimes } from 'react-icons/fa';
+import {FaTimes} from 'react-icons/fa';
 
-const ProductModal = ({ product, onClose }) => {
-  if (!product) return null;
+const ProductModal = ({product, onClose}) => {
+    if (!product) return null;
 
-  const handleOutsideClick = (e) => {
-    if (e.target === e.currentTarget) {
-      onClose();
-    }
-  };
+    const handleOutsideClick = (e) => {
+        if (e.target === e.currentTarget) {
+            onClose();
+        }
+    };
 
-  return (
-    <div
-      className="fixed inset-0 bg-gray-600/75 backdrop-blur-sm flex items-center justify-center z-50 p-4"
-      onClick={handleOutsideClick} 
-    >
-      <div className="bg-white rounded-lg shadow-lg overflow-hidden w-full max-w-5xl max-h-[calc(100vh-3rem)]">
-        <div className="flex justify-between items-center p-4 border-b-white">
-          <h2 className="text-xl font-bold text-[#FF1493]">{product.name}</h2>
-          <button onClick={onClose} className="text-gray-500 hover:text-gray-700">
-            <FaTimes className="w-6 h-6" />
-          </button>
-        </div>
-        <div className="p-6 flex flex-col max-h-[calc(100vh-10rem)] overflow-y-auto"> 
-          <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-            <div className="aspect-square">
-              <img
-                src={product.image}
-                alt={product.name}
-                className="w-full md:h-full  object-cover rounded-lg"
-              />
+    return (
+        <div
+            className="fixed inset-0 bg-gray-600/75 backdrop-blur-sm flex items-center justify-center z-50 p-4"
+            onClick={handleOutsideClick}
+        >
+            <div className="bg-white rounded-lg shadow-lg overflow-hidden w-full max-w-5xl max-h-[calc(100vh-3rem)]">
+                <div className="flex justify-between items-center p-4 border-b-white">
+                    <h2 className="text-xl font-bold text-[#FF1493]">{product.title}</h2>
+                    <button onClick={onClose} className="text-gray-500 hover:text-gray-700">
+                        <FaTimes className="w-6 h-6"/>
+                    </button>
+                </div>
+                <div className="p-6 flex flex-col max-h-[calc(100vh-10rem)] overflow-y-auto">
+                    <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+                        <div className="aspect-square">
+                            <img
+                                src={product.image}
+                                alt={product.title}
+                                className="w-full md:h-full  object-cover rounded-lg"
+                            />
+                        </div>
+                        <div className="space-y-6 mt-6 max-h-[400px] overflow-y-auto">
+                            <h3 className="text-2xl font-semibold">Mahsulot haqida batafsil:</h3>
+                            <p className="text-gray-600 text-lg">{product.description}</p>
+
+                            <div className="max-h-[300px] overflow-y-auto">
+                                <h4 className="text-xl font-medium">Qo'shimcha ma'lumotlar:</h4>
+                                <ul className="list-disc list-inside text-gray-600 text-lg">
+                                    <li>Narxi: $10</li>
+                                    <li>Og'irligi: 500g</li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
-            <div className="space-y-6 mt-6 max-h-[400px] overflow-y-auto"> 
-              <h3 className="text-2xl font-semibold">Mahsulot haqida batafsil:</h3>
-              <p className="text-gray-600 text-lg">{product.description}</p>
-
-              <div className="max-h-[300px] overflow-y-auto"> 
-                <h4 className="text-xl font-medium">Qo'shimcha ma'lumotlar:</h4>
-                <ul className="list-disc list-inside text-gray-600 text-lg">
-                  <li>Narxi: $10</li>
-                  <li>Og'irligi: 500g</li>
-                  <li>Ishlab chiqaruvchi: "..."</li>
-                </ul>
-              </div>
-            </div>
-          </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default ProductModal;
